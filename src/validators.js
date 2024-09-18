ca6f7faa358efbbd650acce48ca0cb29aa13264c
Wilkor <wilkorguitarrista@msn.com>
2024-09-18 17:29:19 -0300

    ajustando api para logar os retornos dtos e lastro

--- src/validators.js
+++ src/validators.js
@@ -1,94 +1,118 @@
 const Joi = require('joi');
 
-// Esquema para o payload com mensagem de texto do WhatsApp
+
+const textPayloadSchema = Joi.object({
+  type: Joi.string().valid('text/plain').required(),
+  content: Joi.string().required(),
+  id: Joi.string().optional(),
+  from: Joi.string().required(),
+  to: Joi.string().required(),
+  metadata: Joi.object({
+    traceparent: Joi.string().required(),
+    '#uniqueId': Joi.string().guid().optional(),
+    '#date_processed': Joi.string().optional(),
+    '#tunnel.originator': Joi.string().required(),
+    '#tunnel.originalFrom': Joi.string().optional(),
+    '#tunnel.originalTo': Joi.string().optional(),
+    '#tunnel.owner': Joi.string().optional(),
+    '#wa.timestamp': Joi.string().optional(),
+    date_created: Joi.string().optional()
+  }).required()
+});
+
+const mediaLinkPayloadSchema = Joi.object({
+  type: Joi.string().valid('application/vnd.lime.media-link+json').required(),
+  content: Joi.object({
+    type: Joi.string().required(),
+    size: Joi.number().required(),
+    uri: Joi.string().uri().required(),
+    title: Joi.string().allow('').required()
+  }).required(),
+  id: Joi.string().optional(),
+  from: Joi.string().required(),
+  to: Joi.string().required(),
+  metadata: Joi.object({
+    '#blipChat.voice': Joi.string().valid('True').optional(),
+    traceparent: Joi.string().required(),
+    '#uniqueId': Joi.string().guid().optional(),
+    '#tunnel.originator': Joi.string().required(),
+    '#date_processed': Joi.string().optional(),
+    '#tunnel.originalFrom': Joi.string().optional(),
+    '#tunnel.originalTo': Joi.string().optional(),
+    '#tunnel.owner': Joi.string().optional(),
+    '#wa.timestamp': Joi.string().optional(),
+    date_created: Joi.string().optional()
+  }).required()
+});
+
+
 const whatsappTextPayloadSchema = Joi.object({
   messaging_product: Joi.string().valid('whatsapp').required(),
   to: Joi.string().required(),
   text: Joi.object({
-    body: Joi.string().min(0).optional() // 'body' pode ser vazio
+    body: Joi.string().optional()
   }).optional(),
   messageID: Joi.string().optional(),
   token: Joi.string().required(),
   contract: Joi.string().required()
 });
 
-// Esquema para o payload com imagem do WhatsApp
 const whatsappImagePayloadSchema = Joi.object({
-  messaging_product: Joi.string().valid('whatsapp').required(),
-  to: Joi.string().required(),
-  image: Joi.object({
-    uri: Joi.string().uri().required(),
-    caption: Joi.string().optional() // 'caption' é opcional
-  }).required(),
-  messageID: Joi.string().optional(),
-  token: Joi.string().required(),
-  contract: Joi.string().required()
-});
-
-// Esquema para o payload que pode conter tanto texto quanto imagem
-const whatsappTextImagePayloadSchema = Joi.object({
-  messaging_product: Joi.string().valid('whatsapp').required(),
-  to: Joi.string().required(),
-  text: Joi.object({
-    body: Joi.string().min(0).optional() // 'body' pode ser vazio
-  }).optional(),
-  image: Joi.object({
-    uri: Joi.string().uri().required(),
-    caption: Joi.string().optional()
-  }).required(),
-  messageID: Joi.string().optional(),
-  token: Joi.string().required(),
-  contract: Joi.string().required()
-});
-
-// Esquema para garantir que o 'body' seja vazio se a imagem estiver presente
-const whatsappPayloadSchema = Joi.object({
   messaging_product: Joi.string().valid('whatsapp').required(),
   to: Joi.string().required(),
   text: Joi.object({
     body: Joi.string().min(0).optional()
   }).optional(),
   image: Joi.object({
-    uri: Joi.string().uri().optional(),
+    link: Joi.string().uri().required()
+  }).optional(),
+  messageID: Joi.string().optional(),
+  token: Joi.string().required(),
+  contract: Joi.string().required()
+});
+
+
+const whatsappImageCaptionPayloadSchema = Joi.object({
+  messaging_product: Joi.string().valid('whatsapp').required(),
+  to: Joi.string().required(),
+  text: Joi.object({
+    body: Joi.string().min(0).optional()
+  }).optional(),
+  image: Joi.object({
+    uri: Joi.string().uri().required(), 
     caption: Joi.string().optional()
   }).optional(),
   messageID: Joi.string().optional(),
   token: Joi.string().required(),
   contract: Joi.string().required()
-})
-  .custom((value, helpers) => {
-    if (value.image && value.text && value.text.body) {
-      return helpers.error('any.custom');
-    }
-    return value;
-  }, 'Imagem e texto');
+});
+
 
 function validatePayload(payload) {
   let validationSchema;
 
-  // Verificar se há tanto texto quanto imagem no payload
-  if (payload.messaging_product === 'whatsapp' && payload.text && payload.image) {
-    validationSchema = whatsappTextImagePayloadSchema; // Valida o payload com texto e imagem
-  } else if (payload.messaging_product === 'whatsapp' && payload.text) {
-    validationSchema = whatsappTextPayloadSchema; // Valida o payload com texto
+  if (payload.messaging_product === 'whatsapp' && payload.text && !payload.image) {
+    validationSchema = whatsappTextPayloadSchema;
   } else if (payload.messaging_product === 'whatsapp' && payload.image) {
-    validationSchema = whatsappImagePayloadSchema; // Valida o payload com imagem
+
+    console.log('aqui')
+    // Valida payload com imagem e legenda
+    validationSchema = whatsappImageCaptionPayloadSchema;
+  } else if (payload.messaging_product === 'whatsapp' && payload.image) {
+    console.log('aqui1')
+    validationSchema = whatsappImagePayloadSchema;
+  } else if (payload.type === 'text/plain') {
+    validationSchema = textPayloadSchema;
+  } else if (payload.type === 'application/vnd.lime.media-link+json') {
+    validationSchema = mediaLinkPayloadSchema;
   } else {
     return { error: `Unsupported Media Type: ${payload.messaging_product || payload.type}`, status: 415 };
   }
-
   const { error } = validationSchema.validate(payload, { abortEarly: false });
   
   if (error) {
     return { error: error.message, status: 400 };
   }
-
-  // Validação personalizada
-  const customError = whatsappPayloadSchema.validate(payload, { abortEarly: false }).error;
-  if (customError) {
-    return { error: 'Campo "body" não deve ser preenchido se a imagem estiver presente', status: 400 };
-  }
-
   return { valid: true };
 }
 
